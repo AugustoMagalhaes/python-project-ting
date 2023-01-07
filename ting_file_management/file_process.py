ecf64969c2469c02f7814ba891c3ea6d9bb10e4c
AugustoMagalhaes <augustommg@gmail.com>
2023-01-07 06:03:56 -0300

    feat: implementa process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+import sys
+
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for index in range(0, len(instance)):
+        if path_file == instance.search(index)["nome_do_arquivo"]:
+            return
+
+    content = txt_importer(path_file)
+
+    file_data = {"nome_do_arquivo": path_file,
+                 "qtd_linhas": len(content),
+                 "linhas_do_arquivo": content}
+
+    sys.stdout.write(str(file_data))
+    instance.enqueue(file_data)
 
 
 def remove(instance):
